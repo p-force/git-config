abe74ad4dd2415476d6a4c67bb13e8521e8ddcc4
Diana Latyrova <109235970+p-force@users.noreply.github.com>
2023-05-26 13:19:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #forc and clone repo поправить, чтобы выполнялось только один раз
-gh repo fork https://github.com/Elbrus-Bootcamp/${1}.git
+gh repo fork https://github.com/${1}.git
 
 #выполнялось один раз
 mv ${1} ~/Desktop/ 
